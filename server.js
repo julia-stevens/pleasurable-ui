406db6e8001f90ef8a0ddeb62638806096a25fb8
julia-stevens <juliastevens02@gmail.com>
2025-05-12 20:55:32 +0200

    #2 Refactor: comments weg

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
-// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+// Import
 import express from 'express'
-
-// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 import methodOverride from "method-override" // Importeer de "method-override" module, die het mogelijk maakt om HTTP-methoden te gebruiken
 
-// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+// Express
 const app = express()
-
-// Maak werken met data uit formulieren iets prettiger
 app.use(express.urlencoded({extended: true}))
-
-// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
-// Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
-// Stel Liquid in als 'view engine'
 // Method Override
 app.use(methodOverride("_method"))
 
@@ -24,11 +15,10 @@ app.use(methodOverride("_method"))
 const engine = new Liquid()
 app.engine('liquid', engine.express())
 
-// Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+// Views
 app.set('views', './views')
 
-
+// Routes
 app.get('/', async function (request, response) {
   response.render('index.liquid')
 })
@@ -45,14 +35,9 @@ app.get('/anouk', async function (request, response) {
   response.render('anouk.liquid')
 })
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+// Port
 app.set('port', process.env.PORT || 8000)
 
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  console.log(`Project draait via http://localhost:${app.get('port')}/\n\nSucces deze sprint. En maak mooie dingen! 🙂`)
+  console.log(`http://localhost:${app.get('port')}`)
 })
-
-// dit is een test 
-// oke hier is nog een aanpassing
