1a9cca4160f9c973e09e2c37d4fa3e3baa1270d8
julia-stevens <juliastevens02@gmail.com>
2025-05-12 22:13:11 +0200

    #18 werkende routes

--- server.js
+++ server.js
@@ -27,11 +27,95 @@ const speakersEndpoint = "speakers"
 const usersEndpoint = "users"
 const webinarsEndpoint = "webinars"
 const messagesEndpoint = "messages"
-const webinarsDetailsEndpoint = "/webinars?fields=*,speakers.*.*,resources.*.*,categories.*.*"
+const webinarsDetailsEndpoint = "webinars?fields=*,speakers.*.*,resources.*.*,categories.*.*"
+const slugFilter = "?filter[slug][_eq]="
 
 // Routes
+// Home
 app.get('/', async function (request, response) {
   response.render('index.liquid')
+})
+
+// Webinars
+app.get('/webinars', async function (request, response) {
+  const webinarsDetailResponse = await fetch(`${api}${webinarsEndpoint}`)
+  const webinarsDetailResponseJSON = await webinarsDetailResponse.json()
+
+  response.render('webinars.liquid', {
+    webinars: webinarsDetailResponseJSON.data
+  })
+})
+
+// Webinars detail
+app.get('/webinars/:slug', async function (request, response) {
+  const slug = request.params.slug 
+  const webinarDetailResponse = await fetch(`${api}${webinarsEndpoint}${slugFilter}${slug}`)
+  const webinarDetailResponseJSON = await webinarDetailResponse.json()
+
+  console.log(webinarDetailResponseJSON.data)
+
+  response.render('webinars-detail.liquid', {
+    webinars: webinarDetailResponseJSON.data
+  })
+})
+
+// Contourings
+app.get('/contourings', async function (request, response) {
+  const contouringsResponse = await fetch(`${api}${contouringsEndpoint}`)
+  const contouringsResponseJSON = await contouringsResponse.json()
+
+  response.render('contourings.liquid', {
+    contourings: contouringsResponseJSON.data
+  })
+})
+
+// Contourings detail
+app.get('/contourings/:slug', async function (request, response) {
+  const slug = request.params.slug
+  const contouringsDetailResponse = await fetch(`${api}${contouringsEndpoint}${slugFilter}${slug}`)
+  const contouringsDetailResponseJSON = await contouringsDetailResponse.json()  
+
+  console.log(contouringsDetailResponseJSON)
+
+  response.render('contourings-detail.liquid', {
+    contourings: contouringsDetailResponseJSON.data
+  })
+})
+
+// Speakers
+app.get('/speakers', async function (request, response) {
+  const speakersResponse = await fetch(`${api}${speakersEndpoint}`)
+  const speakersResponseJSON = await speakersResponse.json()
+
+  response.render('speakers.liquid', {
+    speakers: speakersResponseJSON.data
+  })
+})
+
+// Speakers detail
+app.get('/speakers/:slug', async function (request, response) {
+  const slug = request.params.slug
+  const speakersDetailResponse = await fetch(`${api}${speakersEndpoint}${slugFilter}${slug}`)
+  const speakersDetailResponseJSON = await speakersDetailResponse.json()
+
+  response.render('speakers-detail.liquid', {
+    speakers: speakersDetailResponseJSON.data
+  })
+})
+
+// About us
+app.get('/about-us', async function (request, response) {
+  response.render('about-us.liquid')
+})
+
+// Profile
+app.get('/profile', async function (request, response) {
+  response.render('profile.liquid')
+})
+
+// Profile bookmarks
+app.get('/profile/bookmarks', async function (request, response) {
+  response.render('profile-bookmarks.liquid')
 })
 
 // Port
