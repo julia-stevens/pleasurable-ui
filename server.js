e7d121d83bffe2771501c3a7b0f39e78d7aadb08
ambersr <amber.schalker@hva.nl>
2025-05-16 10:01:34 +0200

    Squashed commit of the following:
    commit 64a6f3c306cb4b34e4b02f55b4044e7de51244c1
    Merge: b1a917a 8d66eaa
    Author: julia-stevens <juliastevens02@gmail.com>
    Date:   Thu May 15 12:30:15 2025 +0200
    
        Merge pull request #35 from julia-stevens/response-fix
    
        Kleine issues in server fix
    
    commit 8d66eaad82d7ab97dab49054ac7d69ddc5fd3774
    Author: Julia Stevens <juliastevens02@gmail.com>
    Date:   Thu May 15 10:25:58 2025 +0200
    
        #34 issues server fix
    
    commit b1a917a8f3bf7f9123f91292cb5a44d90fc8a1e3
    Merge: db0fe04 336bdcb
    Author: halie404 <26465999+halie404@users.noreply.github.com>
    Date:   Thu May 15 10:09:25 2025 +0200
    
        Merge pull request #32 from julia-stevens/server-changes
    
        Server changes
    
    commit 336bdcb2ba7cdf976f82f39fbe6ed229f699a07e
    Author: Halima Dinle <26465999+halie404@users.noreply.github.com>
    Date:   Wed May 14 14:15:28 2025 +0200
    
        #29 server aangepast
    
    commit 255903d572255c5f4ae6b0d88de3fad2e190b7c1
    Author: Halima Dinle <26465999+halie404@users.noreply.github.com>
    Date:   Wed May 14 14:15:06 2025 +0200
    
        #30 mappen aangepast
    
    commit db0fe04a9f138e333905a32b5b02935620643bb5
    Merge: aed6fe5 4432674
    Author: AnoukdeRooij24 <anouk.de.rooij@hva.nl>
    Date:   Wed May 14 11:27:34 2025 +0200
    
        Merge pull request #31 from julia-stevens/css-UI-components
    
        Styling van de links #1
    
    commit 4432674552f834b87e6ffbf1780c1056bc10d2ac
    Author: AnoukdeRooij24 <anouk.de.rooij@hva.nl>
    Date:   Wed May 14 11:19:55 2025 +0200
    
        Styling van de links #1

--- server.js
+++ server.js
@@ -1,126 +1,135 @@
 // Import
-import express from 'express'
-import { Liquid } from 'liquidjs';
-import methodOverride from "method-override" // Importeer de "method-override" module, die het mogelijk maakt om HTTP-methoden te gebruiken
+import express from "express";
+import { Liquid } from "liquidjs";
+import methodOverride from "method-override"; // Importeer de "method-override" module, die het mogelijk maakt om HTTP-methoden te gebruiken
 
 // Express
-const app = express()
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // Method Override
-app.use(methodOverride("_method"))
+app.use(methodOverride("_method"));
 
 // Liquid
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+const engine = new Liquid();
+app.engine("liquid", engine.express());
 
 // Views
-app.set('views', './views')
+app.set("views", "./views");
 
 // API links
-const api = "https://fdnd-agency.directus.app/items/avl_"
-const categoriesEndpoint = "categories"
-const commentsEndpoint = "comments"
-const contouringsEndpoint = "contourings"
-const speakersEndpoint = "speakers"
-const usersEndpoint = "users"
-const webinarsEndpoint = "webinars"
-const messagesEndpoint = "messages"
-const webinarsDetailsEndpoint = "webinars?fields=*,speakers.*.*,resources.*.*,categories.*.*"
-const slugFilter = "?filter[slug][_eq]="
+const directusApiBaseUrl = "https://fdnd-agency.directus.app/items";
+const webinarsEndpoint = `${directusApiBaseUrl}/avl_webinars`;
+const speakersEndpoint = `${directusApiBaseUrl}/avl_speakers`;
+const contouringsEndpoint = `${directusApiBaseUrl}/avl_contourings`;
+
+const commentsEndpoint = `${directusApiBaseUrl}/avl_comments`;
+const usersEndpoint = `${directusApiBaseUrl}/avl_users`;
+const categoriesEndpoint = `${directusApiBaseUrl}/avl_categories`;
+const messagesEndpoint = `${directusApiBaseUrl}/avl_messages`;
+
+const slugFilter = "?filter[slug][_eq]=";
 
 // Routes
 // Home
-app.get('/', async function (request, response) {
-  response.render('index.liquid')
-})
+app.get("/", async function (req, res) {
+  // req + res plss T-T
+  res.render("index.liquid");
+});
 
-// Webinars
-app.get('/webinars', async function (request, response) {
-  const webinarsDetailResponse = await fetch(`${api}${webinarsEndpoint}`)
-  const webinarsDetailResponseJSON = await webinarsDetailResponse.json()
+// webinars
 
-  response.render('webinars.liquid', {
-    webinars: webinarsDetailResponseJSON.data
-  })
-})
+app.get("/webinars", async (req, res) => {
+  const webinarsDetailResponse = await fetch(
+    `${webinarsEndpoint}?fields=*,speakers.*.*,resources.*.*,categories.*.*`
+  );
+  const { data: webinarsDetailResponseJson } =
+    await webinarsDetailResponse.json();
+
+  res.render("webinars.liquid", { webinars: webinarsDetailResponseJson });
+});
 
 // Webinars detail
-app.get('/webinars/:slug', async function (request, response) {
-  const slug = request.params.slug 
-  const webinarDetailResponse = await fetch(`${api}${webinarsEndpoint}${slugFilter}${slug}`)
-  const webinarDetailResponseJSON = await webinarDetailResponse.json()
+app.get("/webinars/:slug", async (req, res) => {
+  const slug = req.params.slug;
+  const webinarDetailResponse = await fetch(
+    `${webinarsEndpoint}${slugFilter}${slug}`
+  );
+  const { data: webinarDetailResponseJSON } =
+    await webinarDetailResponse.json();
 
-  console.log(webinarDetailResponseJSON.data)
-
-  response.render('webinars-detail.liquid', {
-    webinars: webinarDetailResponseJSON.data
-  })
-})
+  res.render("webinars-detail.liquid", {
+    webinars: webinarDetailResponseJSON,
+  });
+});
 
 // Contourings
-app.get('/contourings', async function (request, response) {
-  const contouringsResponse = await fetch(`${api}${contouringsEndpoint}`)
-  const contouringsResponseJSON = await contouringsResponse.json()
+app.get("/contourings", async (req, res) => {
+  const contouringsResponse = await fetch(contouringsEndpoint);
+  const { data: contouringsResponseJSON } = await contouringsResponse.json(); // fetch and json can be a helper function
 
-  response.render('contourings.liquid', {
-    contourings: contouringsResponseJSON.data
-  })
-})
+  res.render("contourings.liquid", {
+    contourings: contouringsResponseJSON,
+  });
+});
 
 // Contourings detail
-app.get('/contourings/:slug', async function (request, response) {
-  const slug = request.params.slug
-  const contouringsDetailResponse = await fetch(`${api}${contouringsEndpoint}${slugFilter}${slug}`)
-  const contouringsDetailResponseJSON = await contouringsDetailResponse.json()  
+app.get("/contourings/:slug", async (req, res) => {
+  const slug = req.params.slug;
+  const contouringsDetailResponse = await fetch(
+    `${contouringsEndpoint}${slugFilter}${slug}`
+  );
+  const { data: contouringsDetailResponseJSON } =
+    await contouringsDetailResponse.json();
 
-  console.log(contouringsDetailResponseJSON)
-
-  response.render('contourings-detail.liquid', {
-    contourings: contouringsDetailResponseJSON.data
-  })
-})
+  res.render("contourings-detail.liquid", {
+    contourings: contouringsDetailResponseJSON,
+  });
+});
 
 // Speakers
-app.get('/speakers', async function (request, response) {
-  const speakersResponse = await fetch(`${api}${speakersEndpoint}`)
-  const speakersResponseJSON = await speakersResponse.json()
+app.get("/speakers", async (req, res) => {
+  const speakersResponse = await fetch(speakersEndpoint);
+  const { data: speakersResponseJSON } = await speakersResponse.json();
 
-  response.render('speakers.liquid', {
-    speakers: speakersResponseJSON.data
-  })
-})
+  res.render("speakers.liquid", {
+    speakers: speakersResponseJSON,
+  });
+});
 
 // Speakers detail
-app.get('/speakers/:slug', async function (request, response) {
-  const slug = request.params.slug
-  const speakersDetailResponse = await fetch(`${api}${speakersEndpoint}${slugFilter}${slug}`)
-  const speakersDetailResponseJSON = await speakersDetailResponse.json()
+app.get("/speakers/:slug", async (req, res) => {
+  const slug = req.params.slug;
+  const speakersDetailResponse = await fetch(
+    `${speakersEndpoint}${slugFilter}${slug}`
+  );
+  const { data: speakersDetailResponseJSON } =
+    await speakersDetailResponse.json();
 
-  response.render('speakers-detail.liquid', {
-    speakers: speakersDetailResponseJSON.data
-  })
-})
+  res.render("speakers-detail.liquid", {
+    speakers: speakersDetailResponseJSON,
+  });
+});
 
 // About us
-app.get('/about-us', async function (request, response) {
-  response.render('about-us.liquid')
-})
+app.get("/about-us", async (req, res) => {
+  res.render("about-us.liquid");
+});
 
 // Profile
-app.get('/profile', async function (request, response) {
-  response.render('profile.liquid')
-})
+app.get("/profile", async (req, res) => {
+  res.render("profile.liquid");
+});
 
 // Profile bookmarks
-app.get('/profile/bookmarks', async function (request, response) {
-  response.render('profile-bookmarks.liquid')
-})
+app.get("/profile/bookmarks", async (req, res) => {
+  res.render("profile-bookmarks.liquid");
+});
 
 // Port
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
-app.listen(app.get('port'), function () {
-  console.log(`http://localhost:${app.get('port')}`)
-})
+app.listen(app.get("port"), function () {
+  console.log(`http://localhost:${app.get("port")}`);
+});
