6129c3a13a8f060ab576f9d42b530f689320c720
Julia Stevens <juliastevens02@gmail.com>
2025-05-22 19:30:27 +0200

    #58 kleine refactor

--- public/script-speaker.js
+++ public/script-speaker.js
@@ -1,8 +1,12 @@
-if ('fetch' in window && 'DOMParser' in window) {
+// global scope
+if ("fetch" in window && "DOMParser" in window) {
+    // callback: op het moment dat submit wordt uigevoerd, dan async function uitvoeren 
     document.addEventListener('submit', async function(event) {
+        // dit is de callback
+        // de callback is een async functie geworden en hiermee kan je andere async functies "awaiten"
         const form = event.target
         
-        if (!form.hasAttribute('data-enhanced')) {
+        if (!form.hasAttribute("data-enhanced")) {
             return
         }
 
@@ -17,8 +21,8 @@ if ('fetch' in window && 'DOMParser' in window) {
         
         const responseText = await response.text()
         const parser = new DOMParser()
-        const responseDOM = parser.parseFromString(responseText, 'text/html')
-        const newState = responseDOM.querySelector('[data-enhanced="' + form.getAttribute('data-enhanced') + '"]')
+        const responseDOM = parser.parseFromString(responseText, "text/html")
+        const newState = responseDOM.querySelector("[data-enhanced='" + form.getAttribute("data-enhanced") + "']")
         
         form.outerHTML = newState.outerHTML
     })
