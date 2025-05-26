4a068bad44e94877141e659ca47b2314500ffe76
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-05-26 13:44:39 +0200

    speakers toegevoegd, comments styling #61 #54 #5

--- server.js
+++ server.js
@@ -104,10 +104,15 @@ app.get("/webinars/:slug", async (req, res) => {
   const commentsDetailResponse = await fetch(`${commentsEndpoint}`);
   const { data: commentsDetailResponseJSON } = await commentsDetailResponse.json();
 
+  const speakersResponse = await fetch(speakersEndpoint);
+  const { data: speakersResponseJSON } = await speakersResponse.json();
+
+
   res.render("webinars-detail.liquid", {
     webinars: webinarDetailResponseJSON,
     categories: categoriesDetailResponseJSON,
     comments: commentsDetailResponseJSON,
+    speakers: speakersResponseJSON,
   });
 });
 
